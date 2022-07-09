55af281616345a4001def225276c729c2c35d59f
Erick Hitter <git-contrib@ethitter.com>
2022-07-09 12:57:39 -0700

    Prepare for tests

---
+++ tests/inc/test-plugin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Test plugin features.
+ *
+ * @package Redis_User_Session_Storage
+ */
+
+namespace Redis_User_Session_Storage\Tests\Inc;
+
+use Redis_User_Session_Storage\Plugin;
+use WP_Session_Tokens;
+use WP_UnitTestCase;
+
+/**
+ * @coversDefaultClass \Redis_User_Session_Storage\Plugin
+ */
+class TestPlugin extends WP_UnitTestCase {
+	protected $plugin;
+
+	public function set_up() {
+		// TODO: use reflection to make this more useful. Create a helper, stop initializing here.
+		$this->plugin = new Plugin( 0 );
+	}
+
+	public function test__construct() {
+		$this->assertInstanceOf( Plugin::class, $this->plugin );
+		$this->assertInstanceOf( WP_Session_Tokens::class, $this->plugin );
+
+		$this->assertEquals( 'wpruss', $this->plugin->prefix );
+	}
+}
